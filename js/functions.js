59d197954799ac52736d9d12306763f8d8ec6b9e
Anna Kuszal <anna.kuszal@o2.pl>
2019-10-09 15:37:51 +0200

    Add html structure & content to sections

---
+++ js/functions.js
@@ -0,0 +1,19 @@
+function printMessage(msg){
+	var div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+}
+
+function printMoves(player, comp) {
+	document.getElementById("move_1").textContent = player;
+	document.getElementById("move_2").textContent = comp;
+  }
+
+function score(player, comp) {
+	document.getElementById("player-score").textContent = player;
+	document.getElementById("comp-score").textContent = comp;
+  }
